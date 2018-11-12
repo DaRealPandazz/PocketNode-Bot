149744a53b075d99dfda2841d1460fa2c2f5fdcc
ThePandazManYT <43769984+ThePandazManYT@users.noreply.github.com>
2018-11-12 01:18:18 -0500

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+ if(message.content.indexOf(process.env.PREFIX)) return;
+ 
+ let sending = new Discord.RichEmbed()
+ .setColor("GREEN")
+ .setTitle("Sending Help Via DMS 📬")
+ 
+ let help = new Discord.RichEmbed()
+ .setColor("GREEN")
+ .setTitle("Showing Help...")
+ 
+ message.channel.send(sending);
+ message.author.send(help);
+ }
+ 
+ module.exports.help = {
+   name: "help"
+}
